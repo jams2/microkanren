10e5ef4e2b105061a75b58d710930391b033a229
jams2 <public@elysee-munn.family>
2023-02-28 20:32:35 +0000

    Add run_constraints

--- src/microkanren.py
+++ src/microkanren.py
@@ -1,11 +1,14 @@
 """
 TODO: maybe add a const_string or symbol type to avoid overhead for strings that won't
 be combined/mutated
+
+TODO: FD constraints could probably be stored as an upper and lower bound instead of a
+set of the entire range
 """
 
 from dataclasses import dataclass
 from functools import reduce, update_wrapper, wraps
-from typing import Union, Tuple, TypeAlias, Any, Callable
+from typing import Union, Tuple, TypeAlias, Any, Callable, Optional
 
 
 @dataclass(slots=True)
@@ -118,18 +121,41 @@ class ReifiedVar:
 Cons: TypeAlias = Union[cons, nil]
 Value: TypeAlias = Union[Var, int, str, bool, Tuple["Value", ...], Cons]
 Substitution: TypeAlias = list[tuple[Var, Value]]
-ConstraintStore: TypeAlias = list[list[tuple[Var, Value]]]
+NeqStore: TypeAlias = list[list[tuple[Var, Value]]]
+DomainStore: TypeAlias = list[tuple[Value, set[int]]]
+Constraint: TypeAlias = Callable[["State"], Optional["State"]]
+ConstraintStore: TypeAlias = list[Constraint]
 
 
 @dataclass(slots=True)
 class State:
     sub: Substitution
-    constraints: ConstraintStore
+    neqs: NeqStore
+    domains: DomainStore
+    fd_cs: ConstraintStore
     var_count: int
 
     @classmethod
     def empty(cls):
-        return cls([], [], 0)
+        return cls([], [], [], [], 0)
+
+    def update(
+        self, *, sub=None, neqs=None, domains=None, fd_cs=None, var_count=None
+    ) -> "State":
+        state = {
+            "sub": self.sub if sub is None else sub,
+            "neqs": self.neqs if neqs is None else neqs,
+            "domains": self.domains if domains is None else domains,
+            "fd_cs": self.fd_cs if fd_cs is None else fd_cs,
+            "var_count": self.var_count if var_count is None else var_count,
+        }
+        return self.__class__(**state)
+
+    def get_domain(self, x: Value) -> tuple[Value, set[int]] | None:
+        return find(x, self.domains)
+
+    def get_relevant_neqs(self, x: Var):
+        return [c for c in self.neqs if find(x, c) is not None]
 
 
 Stream: TypeAlias = Tuple[()] | Callable[[], "Stream"] | Tuple[State, "Stream"]
@@ -181,6 +207,10 @@ def find(x: Var, s: Substitution) -> tuple[Var, Value] | None:
 
 def extend_substitution(x: Var, v: Value, s: Substitution) -> Substitution:
     return [(x, v), *s]
+
+
+def extend_domain(x: Value, fd: set[int], d: DomainStore) -> DomainStore:
+    return [(x, fd), *d]
 
 
 def occurs(x, v, s):
@@ -294,26 +324,29 @@ def verify_eq(new_sub: Substitution, state: State) -> Stream:
         return unit(state)
 
     # There are new associations, so we need to run the constraints
-    remaining_constraints = verify_constraints(state.constraints, [], new_sub)
-    if remaining_constraints is None:
+    remaining_neqs = run_disequality_constraints(state.neqs, [], new_sub)
+    if remaining_neqs is None:
         # A constraint was violated by the new association
         return mzero
 
-    return unit(State(new_sub, remaining_constraints, state.var_count))
+    state_1 = state.update(sub=new_sub, neqs=remaining_neqs)
+    prefix = get_sub_prefix(new_sub, state.sub)
+    state_2 = process_prefix_fd(prefix, state_1.fd_cs)(state_1)
+    return mzero if state_2 is None else unit(state_2)
 
 
-def verify_constraints(
-    existing_constraints: ConstraintStore,
-    verified_constraints: ConstraintStore,
+def run_disequality_constraints(
+    existing_constraints: NeqStore,
+    remaining_constraints: NeqStore,
     sub: Substitution,
-) -> ConstraintStore | None:
+) -> NeqStore | None:
     if existing_constraints == []:
-        return verified_constraints
+        return remaining_constraints
     match unify_all(existing_constraints[0], sub):
         case None:
             # Unification failed, the constraint holds, discard it and continue
-            return verify_constraints(
-                existing_constraints[1:], verified_constraints, sub
+            return run_disequality_constraints(
+                existing_constraints[1:], remaining_constraints, sub
             )
         case new_sub if new_sub == sub:
             # No new associations were made as a result of unification
@@ -323,12 +356,14 @@ def verify_constraints(
             # New associations were added to the sub - constraints were simplified,
             # made more concrete, etc. Keep the simplified constraints and continue
             constraints = get_sub_prefix(new_sub, sub)
-            return verify_constraints(
-                constraints[1:], [constraints, *verified_constraints], sub
+            return run_disequality_constraints(
+                constraints[1:], [constraints, *remaining_constraints], sub
             )
 
 
-def unify_all(constraint, sub: Substitution):
+def unify_all(
+    constraint: list[tuple[Var, Value]], sub: Substitution
+) -> Substitution | None:
     match constraint:
         case []:
             return sub
@@ -361,6 +396,13 @@ def flip(f):
 def neq(pair, *pairs) -> Goal:
     def _neq(state: State) -> Stream:
         u, v = pair
+        # If unification of any of the pairs fails, that pair can never be unified,
+        # so the constraint will always hold
+        # e.g. neq((x,1), (y,2)) in []: both unifications succeed, so the constraints
+        # must be kept
+        # e.g. neq((x,1), (y,2)) in [(x,2)]: the unification of (x,1) will fail,
+        # meaning this unification will never succeed, so this constraint cannot be
+        # violated
         sub = reduce(flip(maybe_unify), pairs, unify(u, v, state.sub))
         return verify_neq(sub, state)
 
@@ -379,7 +421,130 @@ def verify_neq(new_sub: Substitution, state: State) -> Stream:
         # A new mapping was added to the constraint: the constraint has not yet
         # been violated, but may be in future
         constraint = get_sub_prefix(new_sub, state.sub)
-        return unit(State(state.sub, [constraint, *state.constraints], state.var_count))
+        return unit(state.update(neqs=[constraint, *state.neqs]))
+
+
+@dataclass(slots=True)
+class FdConstraint:
+    func: Constraint
+    operands: list[Value]
+
+    def __call__(self, state: State) -> State | None:
+        return self.func(state)
+
+
+def any_relevant_vars(operands, values):
+    match operands:
+        case Var(_) as v:
+            return v in values
+        case [first, *rest]:
+            return any_relevant_vars(first, values) or any_relevant_vars(rest, values)
+        case _:
+            return False
+
+
+def infd(x: Value, domain: set[int]) -> Goal:
+    def _infd(state: State) -> Stream:
+        match process_domain(x, domain)(state):
+            case None:
+                return mzero
+            case _ as s1:
+                return unit(s1)
+        return unit(state)
+
+    return goal(_infd)
+
+
+def process_domain(x: Value, domain: set[int]):
+    def _process_domain(state):
+        match walk(x, state.sub):
+            case Var(_):
+                # x is not associated with any concrete value, update its domain
+                return update_var_domain(x, domain, state)
+            case val if val in domain:
+                # We already have a concrete value, check if it's in the domain
+                return state
+            case _:
+                return None
+
+    return _process_domain
+
+
+def update_var_domain(x: Var, domain: set[int], state: State) -> State | None:
+    match state.get_domain(x):
+        case (_, fd):
+            i = domain & fd
+            if i:
+                return resolve_storable_domain(i, x, state)
+            else:
+                return None
+        case _:
+            return resolve_storable_domain(domain, x, state)
+
+
+def resolve_storable_domain(domain: set[int], x: Var, state: State) -> State | None:
+    if len(domain) == 1:
+        n = domain.copy().pop()
+        next_state = state.update(sub=extend_substitution(x, n, state.sub))
+        return run_constraints([x], state.fd_cs)(next_state)
+    return state.update(domains=extend_domain(x, domain, state.domains))
+
+
+def identity_constraint(state):
+    return state
+
+
+def compose_constraints(f, g):
+    def _compose_constraints(state):
+        maybe_state = f(state)
+        return g(maybe_state) if maybe_state else None
+
+    return _compose_constraints
+
+
+def run_constraints(xs: list, constraints: ConstraintStore):
+    match constraints:
+        case []:
+            return identity_constraint
+        case [first, *rest]:
+            if any_relevant_vars(first, xs):
+                return compose_constraints(
+                    remove_and_run(first),
+                    run_constraints(xs, rest),
+                )
+            else:
+                return run_constraints(xs, rest)
+
+
+def remove_and_run(constraint: FdConstraint) -> Constraint:
+    def _remove_and_run(state: State) -> State | None:
+        if constraint in state.fd_cs:
+            fd_cs = [x for x in state.fd_cs if x != constraint]
+            return constraint(state.update(fd_cs=fd_cs))
+        else:
+            return state
+
+    return _remove_and_run
+
+
+def process_prefix_fd(
+    prefix: Substitution, constraints: ConstraintStore
+) -> Callable[[State], State | None]:
+    if not prefix:
+        return identity_constraint
+    (x, v), *rest_prefix = prefix
+    t = compose_constraints(
+        run_constraints([x], constraints), process_prefix_fd(rest_prefix, constraints)
+    )
+
+    def _process_prefix_fd(state: State) -> State | None:
+        pair = state.get_domain(x)
+        if pair is not None:
+            _, domain_x = pair
+            return compose_constraints(process_domain(v, domain_x), t)(state)
+        return t(state)
+
+    return _process_prefix_fd
 
 
 def get_sub_prefix(new_sub: Substitution, old_sub: Substitution) -> Substitution:
@@ -436,7 +601,7 @@ def fresh(fp: Callable) -> Goal:
     def _fresh(state: State) -> Stream:
         i = state.var_count
         vs = (Var(j) for j in range(i, i + n))
-        return fp(*vs)(State(state.sub, state.constraints, i + n))
+        return fp(*vs)(state.update(var_count=i + n))
 
     return goal(_fresh)
 
@@ -506,14 +671,14 @@ def call_with_empty_state(g: Goal) -> Stream:
 def run(n: int, f_fresh_vars: Callable[[Var, ...], Goal]):
     n_vars = f_fresh_vars.__code__.co_argcount
     fresh_vars = (Var(i) for i in range(n_vars))
-    state = State([], [], n_vars)
+    state = State.empty().update(var_count=n_vars)
     return reify(take(n, f_fresh_vars(*fresh_vars)(state)))
 
 
 def run_all(f_fresh_vars: Callable[[Var, ...], Goal]):
     n_vars = f_fresh_vars.__code__.co_argcount
     fresh_vars = (Var(i) for i in range(n_vars))
-    state = State([], [], n_vars)
+    state = State.empty().update(var_count=n_vars)
     return reify(take_all(f_fresh_vars(*fresh_vars)(state)))
 
 
