7241088a1aa26d86709621e14c4fe3969618526f
Joshua Munn <public@elysee-munn.family>
2023-03-08 16:21:06 +0000

    Move goals to own module

---
+++ src/microkanren/goals.py
@@ -0,0 +1,130 @@
+from microkanren import (
+    Cons,
+    Goal,
+    State,
+    Stream,
+    Var,
+    conj,
+    cons,
+    disj,
+    eq,
+    fail,
+    fresh,
+    goal,
+    ifte,
+    neq,
+    nil,
+)
+
+
+def appendo(xs: Cons | Var, ys: Cons | Var, zs: Cons | Var) -> Goal:
+    return disj(
+        nullo(xs) & eq(ys, zs),
+        fresh(
+            lambda a, d, res: conj(
+                conso(a, d, xs),
+                conso(a, res, zs),
+                appendo(d, ys, res),
+            )
+        ),
+    )
+
+
+def membero(x, xs):
+    return fresh(
+        lambda a, d: conj(
+            conso(a, d, xs),
+            disj(eq(a, x), membero(x, d)),
+        )
+    )
+
+
+def nullo(x):
+    return eq(x, nil())
+
+
+def notnullo(x):
+    return neq(x, nil())
+
+
+def conso(a, d, ls):
+    return eq(cons(a, d), ls)
+
+
+def caro(a, xs):
+    return fresh(lambda d: conso(a, d, xs))
+
+
+def cdro(d, xs):
+    return fresh(lambda a: conso(a, d, xs))
+
+
+def listo(xs):
+    return nullo(xs) | fresh(lambda d: cdro(d, xs) & listo(d))
+
+
+def inserto(x, ys, zs):
+    def _inserto(state: State) -> Stream:
+        return disj(
+            appendo(cons(x, nil()), ys, zs),
+            fresh(
+                lambda a, d, res: conj(
+                    eq((a, d), ys),
+                    eq((a, res), zs),
+                    inserto(x, d, res),
+                ),
+            ),
+        )(state)
+
+    return goal(_inserto)
+
+
+def assoco(x, xs, y):
+    return ifte(
+        eq(xs, nil()),
+        fail(),
+        fresh(
+            lambda key, val, rest: disj(
+                conj(
+                    conso((key, val), rest, xs),
+                    disj(
+                        eq(key, x) & eq((key, val), y),
+                        assoco(x, rest, y),
+                    ),
+                )
+            ),
+        ),
+    )
+
+
+def rembero(x, xs, out):
+    def _rembero(state: State) -> Stream:
+        return disj(
+            nullo(xs) & nullo(out),
+            fresh(
+                lambda a, d, res: disj(
+                    conso(x, d, xs) & eq(d, out),
+                    conj(
+                        neq(a, x),
+                        conso(a, d, xs),
+                        conso(a, res, out),
+                        rembero(x, d, res),
+                    ),
+                )
+            ),
+        )(state)
+
+    return goal(_rembero)
+
+
+def joino(prefix, suffix, sep, out):
+    return disj(
+        nullo(prefix) & eq(suffix, out),
+        nullo(suffix) & eq(prefix, out),
+        fresh(
+            lambda tmp: notnullo(prefix)
+            & notnullo(suffix)
+            & appendo(prefix, sep, tmp)
+            & appendo(tmp, suffix, out)
+        ),
+    )
