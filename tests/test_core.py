784b0ddb912a895c012c6af626308147c13f95b3
Joshua Munn <public@elysee-munn.family>
2024-10-27 17:54:23 +0000

    fix: Add occurs check to extend_substitution

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,115 +1,48 @@
+import pytest
 from fastcons import cons, nil
-from pyrsistent import pmap
 
 from microkanren import (
-    ReifiedVar,
+    OccursError,
     Var,
-    disj,
     empty_sub,
-    eq,
     extend_substitution,
-    fresh,
-    get_sub_prefix,
-    run,
-    run_all,
-    snooze,
     walk,
 )
-from microkanren.goals import appendo
-from microkanren.utils import _
 
 
-class TestSubstitution:
-    def test_extend_substitution(self):
-        val = object()
-        s = extend_substitution(Var(0), val, empty_sub())
-        assert walk(Var(0), s) is val
-
-    def test_walk_self(self):
-        assert walk(Var(0), empty_sub()) == Var(0)
-
-    def test_walk_constant(self):
-        assert walk("foo", empty_sub()) == "foo"
-
-    def test_recursive_walk(self):
-        val = object()
-        s = extend_substitution(
-            Var(0), Var(1), extend_substitution(Var(1), val, empty_sub())
-        )
-        assert walk(Var(0), s) is val
-
-    def test_get_sub_prefix(self):
-        x = object()
-        initial = empty_sub()
-        a = extend_substitution(Var(0), 1, initial)
-        b = extend_substitution(
-            Var(2),
-            x,
-            extend_substitution(Var(1), Var(2), a),
-        )
-        assert set(get_sub_prefix(b, a).items()) == set(
-            pmap({Var(1): Var(2), Var(2): x}).items()
-        )
+def test_extend_substitution():
+    val = object()
+    s = extend_substitution(Var(0), val, empty_sub())
+    assert walk(Var(0), s) is val
 
 
-class TestEq:
-    def test_simple_eq(self):
-        result = run_all(lambda x: eq(x, 1))
-        assert result == [1]
+def test_walk_unbound_var():
+    assert walk(Var(0), empty_sub()) == Var(0)
 
 
-def fives(x):
-    return eq(x, 5) | snooze(fives, x)
+def test_walk_unbound_value():
+    assert walk("foo", empty_sub()) == "foo"
 
 
-def sixes(x):
-    return eq(x, 6) | snooze(sixes, x)
+def test_recursive_walk():
+    val = object()
+    s = extend_substitution(
+        Var(0), Var(1), extend_substitution(Var(1), val, empty_sub())
+    )
+    assert walk(Var(0), s) is val
 
 
-def test_snooze():
-    result = run(3, lambda x: fives(x))
-    assert result == [5, 5, 5]
-
-    result = run(8, lambda x: fives(x) | sixes(x))
-    assert result == [5, 6, 5, 6, 5, 6, 5, 6]
-
-
-def test_recursion():
-    # Check we don't blow python's stack
-    assert len(run(10000, lambda x: fives(x))) == 10000
-
-
-def test_disj_interleaving():
-    # Test that the order of results matches examples from the miniKanren paper
-
-    def function_disj_relation(x):
-        return disj(eq(x, 1), eq(x, 2), eq(x, 3), snooze(function_disj_relation, x))
-
-    assert run(6, lambda x: function_disj_relation(x)) == [1, 2, 3, 1, 2, 3]
-
-    def operator_disj_relation(x):
-        return eq(x, 1) | eq(x, 2) | eq(x, 3) | snooze(operator_disj_relation, x)
-
-    assert run(6, lambda x: function_disj_relation(x)) == [1, 2, 3, 1, 2, 3]
-
-    def patho(x, y):
-        return arco(x, y) | fresh(lambda z: arco(x, z) & patho(z, y))
-
-    def arco(x, y):
-        return disj(
-            eq("a", x) & eq("b", y),
-            eq("b", x) & eq("a", y),
-            eq("b", x) & eq("d", y),
-        )
-
-    assert "".join(run(9, lambda x: patho("a", x))) == "badbadbad"
-
-
-def test_vars_reified_correctly():
-    # Test the reification of fresh variables matches a known example (appendo)
-
-    R = ReifiedVar
-    result = run(3, lambda x, y, z: appendo(x, y, z))
-    assert result[0] == (nil(), R(0), R(0))
-    assert result[1] == (_(R(0)), R(1), cons(R(0), R(1)))
-    assert result[2] == (_(R(0), R(1)), R(2), cons(R(0), cons(R(1), R(2))))
+@pytest.mark.parametrize(
+    "val",
+    [
+        Var(0),
+        cons(Var(0), nil()),
+        (Var(0), Var(0)),
+        [Var(0)],
+        [[Var(0)]],
+        ([Var(0)],),
+    ],
+)
+def test_occurs_check_raises(val):
+    with pytest.raises(OccursError):
+        extend_substitution(Var(0), val, empty_sub())
